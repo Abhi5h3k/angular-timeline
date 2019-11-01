a9bcb9a3463f107b7aace44408f4ef9a19e21a14
Abhi <37928721+Abhi5h3k@users.noreply.github.com>
2019-11-01 11:41:18 +0530

    added tags

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -7,18 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
   itemlist:TimeLineItems[]=[];
+  tags:string[]=['Live','love','laught'];
   
   constructor() { }
 
   ngOnInit() {
     console.log(typeof this.itemlist);
-    this.itemlist.push(new TimeLineItems(1,new Date().toLocaleTimeString(),"Title","Some quick example text to build on the card title and make up the bulk of the card's content."));
+    this.itemlist.push(new TimeLineItems(1,new Date().toLocaleTimeString(),"Title","Some quick example text to build on the card title and make up the bulk of the card's content.",this.tags));
 
   }
 
   onClick(){
     console.log(this.itemlist.length);
-    this.itemlist.push(new TimeLineItems(this.itemlist.length+1,new Date().toLocaleTimeString(),"Title","Some quick example text to build on the card title and make up the bulk of the card's content."));
+    this.itemlist.push(new TimeLineItems(this.itemlist.length+1,new Date().toLocaleTimeString(),"Title","Some quick example text to build on the card title and make up the bulk of the card's content.",this.tags));
     console.log("added one more"+this.itemlist.length);
   }
 }
@@ -28,13 +29,21 @@ class TimeLineItems{
   time:string;
   title:string;
   content:string;
-
-  constructor(id:number,time:string,title:string,content:string) {
+  tags:string[]=[];
+  colors:string[]=['badge-primary','badge-secondary','badge-success','badge-danger','badge-warning','badge-info','badge-dark'];
+  rand:number;
+  constructor(id:number,time:string,title:string,content:string,tags:string[]) {
       this.id=id;
       this.time=time;
       this.title=title;
       this.content=content;
+      this.tags=tags;
+      this.rand=Math.floor(Math.random() * 6);
+      
    }
 
+   getRandColor(){
+     return this.colors[Math.floor(Math.random() * 6)];
+   }
 
 }
